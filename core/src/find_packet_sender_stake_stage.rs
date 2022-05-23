d3da0ba39d139cd4f443eb988588d02e4ff071d1
Christian Kamm <mail@ckamm.de>
2022-05-23 21:27:20 +0200

    findpacketsenderstake: add discard after receive (#25458)
    This mimics a similar change in sigverify, see #25388
    
    (cherry picked from commit 6429aff13b0cff4f11b17b575365cc6c4ba3df35)
    
    # Conflicts:
    #	core/src/find_packet_sender_stake_stage.rs

--- core/src/find_packet_sender_stake_stage.rs
+++ core/src/find_packet_sender_stake_stage.rs
@@ -26,6 +26,11 @@ thread_local!(static PAR_THREAD_POOL: RefCell<ThreadPool> = RefCell::new(rayon::
                     .build()
                     .unwrap()));
 
+// Try to target 50ms, rough timings from mainnet machines
+//
+// 50ms/(1us/packet) = 50k packets
+const MAX_FINDPACKETSENDERSTAKE_BATCH: usize = 50_000;
+
 pub type FindPacketSenderStakeSender = Sender<Vec<PacketBatch>>;
 pub type FindPacketSenderStakeReceiver = Receiver<Vec<PacketBatch>>;
 
@@ -38,6 +43,8 @@ struct FindPacketSenderStakeStats {
     receive_batches_time: u64,
     total_batches: u64,
     total_packets: u64,
+    total_discard_random: usize,
+    total_discard_random_time_us: usize,
 }
 
 impl FindPacketSenderStakeStats {
@@ -65,6 +72,12 @@ impl FindPacketSenderStakeStats {
                 ),
                 ("total_batches", self.total_batches as i64, i64),
                 ("total_packets", self.total_packets as i64, i64),
+                ("total_discard_random", self.total_discard_random, i64),
+                (
+                    "total_discard_random_time_us",
+                    self.total_discard_random_time_us,
+                    i64
+                ),
             );
             *self = FindPacketSenderStakeStats::default();
             self.last_print = now;
@@ -87,6 +100,7 @@ impl FindPacketSenderStakeStage {
         let mut stats = FindPacketSenderStakeStats::default();
         let thread_hdl = Builder::new()
             .name("find-packet-sender-stake".to_string())
+<<<<<<< HEAD
             .spawn(move || {
                 let mut last_stakes = Instant::now();
                 let mut ip_to_stake: HashMap<IpAddr, u64> = HashMap::new();
@@ -108,6 +122,28 @@ impl FindPacketSenderStakeStage {
                             let num_batches = batches.len();
                             let mut apply_sender_stakes_time =
                                 Measure::start("apply_sender_stakes_time");
+=======
+            .spawn(move || loop {
+                match streamer::recv_packet_batches(&packet_receiver) {
+                    Ok((mut batches, num_packets, recv_duration)) => {
+                        let num_batches = batches.len();
+
+                        let mut discard_random_time =
+                            Measure::start("findpacketsenderstake_discard_random_time");
+                        let non_discarded_packets = solana_perf::discard::discard_batches_randomly(
+                            &mut batches,
+                            MAX_FINDPACKETSENDERSTAKE_BATCH,
+                            num_packets,
+                        );
+                        let num_discarded_randomly =
+                            num_packets.saturating_sub(non_discarded_packets);
+                        discard_random_time.stop();
+
+                        let mut apply_sender_stakes_time =
+                            Measure::start("apply_sender_stakes_time");
+                        let mut apply_stake = || {
+                            let ip_to_stake = staked_nodes.read().unwrap();
+>>>>>>> 6429aff13 (findpacketsenderstake: add discard after receive (#25458))
                             Self::apply_sender_stakes(&mut batches, &ip_to_stake);
                             apply_sender_stakes_time.stop();
 
@@ -117,6 +153,7 @@ impl FindPacketSenderStakeStage {
                             }
                             send_batches_time.stop();
 
+<<<<<<< HEAD
                             stats.apply_sender_stakes_time = stats
                                 .apply_sender_stakes_time
                                 .saturating_add(apply_sender_stakes_time.as_us());
@@ -136,6 +173,23 @@ impl FindPacketSenderStakeStage {
                             StreamerError::RecvTimeout(RecvTimeoutError::Timeout) => (),
                             _ => error!("error: {:?}", e),
                         },
+=======
+                        stats.apply_sender_stakes_time = stats
+                            .apply_sender_stakes_time
+                            .saturating_add(apply_sender_stakes_time.as_us());
+                        stats.send_batches_time = stats
+                            .send_batches_time
+                            .saturating_add(send_batches_time.as_us());
+                        stats.receive_batches_time = stats
+                            .receive_batches_time
+                            .saturating_add(recv_duration.as_nanos() as u64);
+                        stats.total_batches =
+                            stats.total_batches.saturating_add(num_batches as u64);
+                        stats.total_packets =
+                            stats.total_packets.saturating_add(num_packets as u64);
+                        stats.total_discard_random_time_us += discard_random_time.as_us() as usize;
+                        stats.total_discard_random += num_discarded_randomly;
+>>>>>>> 6429aff13 (findpacketsenderstake: add discard after receive (#25458))
                     }
 
                     stats.report(name);
